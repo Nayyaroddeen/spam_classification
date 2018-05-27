21b5ae8b319b7879558f42ac0f272c39f0eb6dd2
Sayyad Nayyaroddeen <nayyaralisha12345@gmail.com>
2018-05-27 18:07:35 +0530

    added code for cnn testing

--- test_models.py
+++ test_models.py
@@ -3,7 +3,7 @@ import pickle
 from sklearn.metrics import accuracy_score
 from bulid_models import count_vector_model
 from keras.models import model_from_json
-
+import numpy as np
 def transfrom_word_cound_model(data):
     count_vec=pickle.load(open('best_models/count_vec_model.sav', 'rb'))
     return count_vec.transform(data)
@@ -43,6 +43,28 @@ def test_nn_model(data,class_list):
 
     validation_acc = accuracy_score(class_list, predict_out)
     print(validation_acc)
+
+def test_cnn_model(data,class_list):
+    data = np.expand_dims(data, axis=2)
+    json_file = open('best_models/cnn_model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("best_models/cnn_model.h5")
+    print("Loaded model from disk")
+    loaded_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    predict = loaded_model.predict(data)
+    predict_out = []
+    for i in range(0, len(data)):
+        if (predict[i] > 0.25):
+            predict_out.append(1)
+        else:
+            predict_out.append(0)
+
+    validation_acc = accuracy_score(class_list, predict_out)
+    print(validation_acc)
+
 if __name__ == "__main__":
     class_list=[]
     a=[]
@@ -51,5 +73,5 @@ if __name__ == "__main__":
     count_vec=transfrom_word_cound_model(count_vec)
     #stest_logist_model(count_vec.toarray().astype(int), class_list)
     #test_random_forest_model(count_vec.toarray().astype(int), class_list)
-    test_nn_model(count_vec.toarray().astype(int),class_list)
-    
+    #test_nn_model(count_vec.toarray().astype(int),class_list)
+    test_cnn_model(count_vec.toarray().astype(int), class_list)
